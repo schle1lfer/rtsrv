a0fdc8ee83c9c700022b7edc5ed1ab04f4385a66
Denis SHashunkin <schle1lfer@gmail.com>
2026-05-14 08:16:31 +0300

    Updated 5k/15k configs

--- scripts/run-srmd-hwasic-15k.sh
+++ scripts/run-srmd-hwasic-15k.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./server-local/sbin/srmd \
-  --sot ./server-local/etc/srmd/route_sot_v3_hwasic-15k.json \
+  --sot ./server-local/etc/srmd/route_sot_5000_new.json \
   --config ./server-local/etc/srmd/srmd.json \
   --foreground
